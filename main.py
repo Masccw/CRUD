6a6e365c4c03ba7c368669136415d9bf9ea2f73a
Marina Carvalho <masocacads@gmail.com>
2025-09-05 12:36:49 -0300

    Esqueleto do menu pronto e parte da database feita

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 #Arquivo principal com o menu
-from import
-from import
-from import
-from import
-from import
+from adicionar import adicionar_curso
+from pesquisar import pesquisar_curso
+from edit import edit_curso
+from remove import remove_curso
+from database import list_curso
 
 while True:
-    opcao_curso = int(input("1 - Adicionar curso\n" \         #cadastrar um novo curso (nome, du ração, professor, etc.)
-                            "2 - Pesquisar curso\n" \         #procurar curso por nome ou código   
-                            "3 - Editar curso\n"\           #atualizar informações de um curso existente
-                            "4 - Remover curso\n"\           #excluir curso do sistema
-                            "5 - Listar todos os cursos\n"\
-                            "6 - Sair → encerrar o programa"))
+                           # 1 - cadastrar um novo curso (nome, duração, professor, etc.)
+                           # 2 - procurar curso por nome ou código
+                           # 3 - atualizar informações de um curso existente
+                           # 4 - excluir curso do sistema
+    opcao_curso = int(input("1 - Adicionar curso\n"          
+                            "2 - Pesquisar curso\n"       
+                            "3 - Editar curso\n"         
+                            "4 - Remover curso\n"         
+                            "5 - Listar todos os cursos\n"
+                            "6 - Sair → encerrar o programa\n"
+                            "Escolha uma opção: "
+                            ))
     if opcao_curso == 1:
         adicionar_curso()
     elif opcao_curso == 2:
         pesquisar_curso()
     elif opcao_curso == 3:
-        remover_curso()
+        edit_curso()
     elif opcao_curso == 4:
         remove_curso()
     elif opcao_curso == 5:
@@ -25,7 +31,7 @@ while True:
     elif opcao_curso == 6:
         break
     else:
-        print("Opção inválida")
+        print("Opção inválida. Tente novamente")
      
     
 
