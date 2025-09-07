5e555f638f81e37735240e791eecfbb0a225ec9b
danirfs22 <121183358+danirfs22@users.noreply.github.com>
2025-09-07 16:54:31 +0000

    Função: Adicionar itens

--- adicionar.py
+++ adicionar.py
@@ -1,1 +1,13 @@
 #Função de adicionar items
+from database import cursos
+
+def adicionar_curso(cursos, nome, descricao, carga):
+    novo_id = cursos[-1]["id"] + 1 if cursos else 1  
+    novo_curso = {
+        "id": novo_id,
+        "nome": nome,
+        "descrição": descricao,
+        "carga": carga
+    }
+    cursos.append(novo_curso)
+    print(f"Curso '{nome}' adicionado com sucesso!")
